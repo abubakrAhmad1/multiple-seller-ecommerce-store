098b71f28f08207f06c464874e4c85b4699f6418
abubakrAhmad1 <abubakarahmad0045@gmail.com>
2025-04-25 13:11:15 -0700

    feat : show already added products

--- frontend/src/components/AddProducts/index.jsx
+++ frontend/src/components/AddProducts/index.jsx
@@ -1,6 +1,13 @@
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { getProducts } from "../../redux/slice/loginUser";
+import { useEffect } from "react";
+
 export default function AddProducts() {
+
+  useEffect(()=>{
+    getProducts();
+  });
   const sellerProducts = useSelector(
     (state) => state.addProducts.storedProducts
   );
@@ -12,10 +19,10 @@ export default function AddProducts() {
       <button onClick={() => navigate("/addProductDiscription")}>
         Click To Add Product
       </button>
-      {sellerProducts.map((product) => (
+      {LoginUser.products.map((product) => (
         <div>
           {/* DISPLAY ALL PRPODUCTS OF THE SELLER */}
-          {product}
+          {product.title}
         </div>
       ))}
     </div>
--- frontend/src/redux/slice/loginUser.js
+++ frontend/src/redux/slice/loginUser.js
@@ -1,4 +1,12 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getProducts = createAsyncThunk("loginUserSlice", async (ThunkAPI) => {
+  const state = ThunkAPI.getState();
+  const{products} =state;
+   const latestProducts = await axios.get( `http://localhost:8000/seller/products`);
+   return latestProducts;
+});
 
 const loginUser = createSlice({
   name: "loginUser",
@@ -7,6 +15,7 @@ const loginUser = createSlice({
     id: "",
     type: "",
     images: [],
+    products : []
   },
   reducers: {
     setUser: (state, action) => {
@@ -15,10 +24,13 @@ const loginUser = createSlice({
       state.type = action.payload.type || "";
       state.images = action.payload.images ? [...action.payload.images] : [];
     },
-    addProduct : (state, action) => {
-      
-    }
+    addProduct: (state, action) => {},
   },
+  extraReducers : (builder) => {
+    builder.addCase(getProducts.fulfilled , (state,action)=>{
+      state.products = action.payload;
+    })
+  }
 });
 
 export const { setUser } = loginUser.actions;
